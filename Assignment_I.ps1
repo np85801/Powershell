5a9ba7dcbb7d47e23d70a666cac098bf02a4c756
kalpeshdhotre <49611334+kalpeshdhotre@users.noreply.github.com>
2019-12-03 10:58:47 +0530

    Update Assignment_I.ps1

--- Assignment_I.ps1
+++ Assignment_I.ps1
@@ -1,6 +1,16 @@
-# Command to get %temp% 
-Get-PSProvider -PSProvider Environment
-Get-Childitem env:
+# Task By NTMS 
+# 1) In the Registry, go to HKEY_CURRENT_USER\software\microsoft\Windows\-currentversion\explorer. Locate the Advanced key, and set its DontPrettyPathproperty to 1
+Set-ItemProperty -Path 'HKCU:\software\microsoft\Windows\currentversion\explorer' -Name 'DontPrettyPathProperty' -Value '1'
 
-# if we run these two commands sequentially it gives different output, how to filter the same?
-Get-PSProvider -PSProvider Environment; Get-Childitem env:
+# 2) Create a new directory called C:\Demo.
+New-Item -Path "c:\" -ItemType "directory" -Name "Demo"
+
+# 3) Create a zero-length file named C:\Demo\Test.txt 
+New-Item -Path "c:\Demo" -ItemType "file" -Name "Test.txt"
+
+# 4) Try to change the content of the Test.txt file
+Add-Content -Path "c:\demo\test.txt" -Value "Hello My First assignment"
+
+# 5) Display Value of %temp%
+Get-PSProvider -PSProvider Environment
+Get-Childitem env:temp
